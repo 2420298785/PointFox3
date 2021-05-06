d422ef28acb1a588a009c013db98eb5911de0eea
gitl <2420298785@qq.com>
2021-05-06 09:57:56 +0800

    '增加public、src目录'

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="footerWrapper">
+<el-footer id="footerBookWrapper">
+                <div id="footerTips">
+                    <div id="tip1">
+                        <span>通信地址：江西省南昌市湾里区江西软件大学 邮编330041</span>
+                    </div>
+                    <div id="tip2">
+                        <span>&copy;版权属于江西软件大学</span>
+                    </div>
+                </div>
+            </el-footer>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#footerTips{
+  text-align: center;
+margin-top: 50px;
+}
+</style>
